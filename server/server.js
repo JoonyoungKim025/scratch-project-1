9cd4abf2a80ffcb92e9cfd1c841ce0e19f681775
Tommy Song <TommySong@Tommys-MacBook-Air.local>
2020-09-15 19:35:53 -0700

    working on socket

--- server/server.js
+++ server/server.js
@@ -2,22 +2,55 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
-const path = require('path');
-const bodyParser = require('body-parser');
-const api = require('./routes/api');
-const cors = require('cors');
+const https = require("https");
+const path = require("path");
+const bodyParser = require("body-parser");
+const api = require("./routes/api");
+const cors = require("cors");
+const server = https.createServer(app);
+
+// requiring socket library
+const socketIo = require("socket.io");
+// apply socket IO to our server
+const io = socketIo(server);
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Send that bad boy to api. Thanks for all that you do, api.
-app.use('/api', api)
+// Socket Connection
+io.on("connection", (socket) => {
+  // when socket has connected we log New WS Connection
+  console.log("New WS Connection...");
 
-// Sometimes you're gonna have static files. 
+  // emit to everyone except the user
+  // Broadcast when a user connects
+  socket.broadcast.emit("message", "A user has joined the chat!");
+
+  // Runs when client disconnects
+  socket.on("disconnect", () => {
+    // tells everybody
+    io.emit("message", " As user has left the chat");
+  });
+
+  // only emits to the single/current client
+  // this message should popup if it connects to the front end
+  socket.emit("message", "Welcome to the chat!");
+
+  // Listen for chat
+  socket.on("chatMessage", (msg) => {
+    // send back message emitted by the client back for display
+    io.emit("message", msg);
+  });
+});
+
+// Send that bad boy to api. Thanks for all that you do, api.
+app.use("/api", api);
+
+// Sometimes you're gonna have static files.
 // During these times this router will serve them up.
 // She's small but mighty.
-app.use('/', express.static('../index.html'));
+app.use("/", express.static("../index.html"));
 
 // Is the user lost? Silly user.
 // Catch all error handler will tell them they lost af
@@ -27,18 +60,17 @@ app.use((req, res) => res.sendStatus(404));
 // The one. The only. The Global Error Handler.
 app.use((err, req, res, next) => {
   const defaultErr = {
-    log: 'Express error handler caught unknown middleware error',
+    log: "Express error handler caught unknown middleware error",
     status: 500,
-    message: { err: ' An error occurred' },
+    message: { err: " An error occurred" },
   };
   const errorObj = Object.assign({}, defaultErr, err);
   console.log(errorObj.log);
   return res.status(errorObj.status).json(errorObj.message);
 });
 
-app.listen(PORT, () => {
-    console.log(`Server listening on port: ${PORT}`);
+server.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
 });
 
 module.exports = app;
-
